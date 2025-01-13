1bf6dbb7e963552b27ffc18a5235ef8c9d58322d
sasha <sasha.park.designer@gmail.com>
2025-01-13 11:37:56 +0900

    이상해씨만 데이터 가져오기 실습

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,22 @@ function App() {
   useEffect(() => {
   const fetchAPI = async () => {
 
-    const response = await fetch('https://pokeapi.co/api/v2')
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon-species/1/')
     const data = await response.json()
+    console.log(data.flavor_text_entries.find(el => el.language.name === 'ko').flavor_text)
     console.log(data)
     
       // .then(response => response.json())
       // .then(data => console.log(data))
+
+      const pokemonData = {
+        id: 1,
+        name: data.names.find(el => el.language.name === 'ko').name,
+        description: data.flavor_text_entries.find(el => el.language.name === 'ko').flavor_text,
+        front: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+        back:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png',
+      }
+      console.log(pokemonData)
    }
    fetchAPI();
   }, [])
