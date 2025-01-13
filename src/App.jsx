9f9912954df4bced57023d285e9ab022344acace
sasha <sasha.park.designer@gmail.com>
2025-01-13 11:53:38 +0900

    151개 불러오기

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,39 @@ function App() {
 
   // 비동기 함수를 이용하여 API를 호출하고 데이터를 가져온다.
   useEffect(() => {
-  const fetchAPI = async () => {
 
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon-species/1/')
-    const data = await response.json()
-    console.log(data.flavor_text_entries.find(el => el.language.name === 'ko').flavor_text)
-    console.log(data)
-    
-      // .then(response => response.json())
-      // .then(data => console.log(data))
+    const numberArray = Array.from({ length: 151 }, (_, i) => i + 1)
+    console.log(numberArray)
 
-      const pokemonData = {
-        id: 1,
-        name: data.names.find(el => el.language.name === 'ko').name,
-        description: data.flavor_text_entries.find(el => el.language.name === 'ko').flavor_text,
-        front: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
-        back:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png',
-      }
-      console.log(pokemonData)
-   }
-   fetchAPI();
+    const fetchAPI = async (pokemonId) => {
+
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonId}/`)
+      const data = await response.json()
+      console.log(data.flavor_text_entries.find(el => el.language.name === 'ko').flavor_text)
+      console.log(data)
+      
+        // .then(response => response.json())
+        // .then(data => console.log(data))
+
+        const pokemonData = {
+          id: pokemonId,
+          name: data.names.find(el => el.language.name === 'ko').name,
+          description: data.flavor_text_entries.find(el => el.language.name === 'ko').flavor_text,
+          front: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`,
+          back:`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${pokemonId}.png`,
+        }
+        return pokemonData
+    }
+
+    const fetchPokemonDatas = async () => { 
+
+      const pokemonDatas = await Promise.all(numberArray.map(el => 
+        fetchAPI(el)))  // Promise.all()을 이용하여 비동기 함수를 동기적으로 실행한다.
+      console.log(pokemonDatas)
+
+    }
+
+    fetchPokemonDatas()
   }, [])
 
   return (
