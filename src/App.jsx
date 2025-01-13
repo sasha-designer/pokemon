6a6edc96ee26d378de8ece95fc15c71de1d457a7
sasha <sasha.park.designer@gmail.com>
2025-01-13 15:12:48 +0900

    Main, Detail 페이지 작성

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.scss'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchMultiplePokemonById } from './RTK/thunk'
+import { Routes, Route } from 'react-router-dom'
+import Main from './pages/Main'
+import Detail from './pages/Detail'
+import Search from './pages/Search'
+import Favorite from './pages/Favorite'
+import { Link } from 'react-router-dom'
+
 
 function App() {
 
 
   const dispatch = useDispatch()
-  const pokemonData = useSelector(state => state.pokemon)
-  console.log(pokemonData)
+
 
   useEffect(() => {
     dispatch(fetchMultiplePokemonById(151))
@@ -20,6 +26,21 @@ function App() {
   return (
     <>
       <h1 className='text-[40px] text-center'>포케몬 도감</h1>
+      <nav className='flex gap-[10px] justify-center'>
+        <Link to='/'>메인</Link>
+        <Link to='/detail/1'>상세정보</Link>
+        <Link to='/search'>검색</Link>
+        <Link to='/favorite'>찜목록</Link>
+      </nav>
+      <main className='flex flex-wrap gap-[20px] justify-center pt-[20px]'>
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/detail/:pokemonId' element={<Detail />} />
+          <Route path='/search' element={<Search />} />
+          <Route path='/favorite' element={<Favorite />} />
+        </Routes>
+      </main>
+      
     </>
   )
 }
