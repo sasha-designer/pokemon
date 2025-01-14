93a359e6903633a59ff1be56b3640ab984f75dfb
sasha <sasha.park.designer@gmail.com>
2025-01-14 10:43:28 +0900

    서치페이지 완성

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,6 +2,7 @@ import { getRegExp } from "korean-regexp"
 import { useSelector } from "react-redux"
 import { useSearchParams } from "react-router-dom"
 import { selectPokemonByRegExp } from "../RTK/selector"
+import { Card } from "../component/Card"
 
 
 export default function Search() {
@@ -13,8 +14,8 @@ export default function Search() {
     const pokemon = useSelector(selectPokemonByRegExp(reg))
     console.log(pokemon)
     return (
-        <div>
-            <h1>Search</h1>
-        </div>
+        <>
+            {pokemon.map(el => <Card key={el.id} pokemon={el} />)}
+        </>
     )
 }
