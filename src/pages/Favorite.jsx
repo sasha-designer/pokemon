f5c4d06eb4716def9c437251b1f16b312b6738e0
sasha <sasha.park.designer@gmail.com>
2025-01-14 11:25:58 +0900

    찜 페이지 작성

--- src/pages/Favorite.jsx
+++ src/pages/Favorite.jsx
@@ -1,7 +1,12 @@
+import { useSelector } from "react-redux"
+import { selectFavoritePokemons } from "../RTK/selector"
+import { Card } from "../component/Card"
+
 export default function Favorite() {
+    const pokemon = useSelector(selectFavoritePokemons)
     return (
-        <div>
-            <h1>Favorite</h1>
-        </div>
+         <>
+            {pokemon.map(el => <Card key={el.id} pokemon={el} />)}
+        </>
     )
 }
