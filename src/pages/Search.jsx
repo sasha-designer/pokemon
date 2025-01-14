7f25a1c9e614458f015afebb2377402138ea816f
sasha <sasha.park.designer@gmail.com>
2025-01-14 10:34:18 +0900

    Search 페이지에 useSearchParams 설정

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,4 +1,11 @@
+import { useSearchParams } from "react-router-dom"
+
+
 export default function Search() {
+
+    const [searchParams] = useSearchParams()
+    const param = searchParams.get('pokemon')
+    console.log(param)
     return (
         <div>
             <h1>Search</h1>
