2699ac49d9db63927de41ad7dab1425e59ca038b
sasha <sasha.park.designer@gmail.com>
2025-01-14 10:41:23 +0900

    Search페이지에서 useSelector를 이용하여 정규식을 받는 코드 작성

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,7 @@
 import { getRegExp } from "korean-regexp"
+import { useSelector } from "react-redux"
 import { useSearchParams } from "react-router-dom"
+import { selectPokemonByRegExp } from "../RTK/selector"
 
 
 export default function Search() {
@@ -7,6 +9,9 @@ export default function Search() {
     const [searchParams] = useSearchParams()
     const param = searchParams.get('pokemon')
     const reg = getRegExp(param)
+
+    const pokemon = useSelector(selectPokemonByRegExp(reg))
+    console.log(pokemon)
     return (
         <div>
             <h1>Search</h1>
