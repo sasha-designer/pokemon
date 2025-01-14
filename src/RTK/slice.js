42ed5f11b2e14d287143efd7b141617f43ab3cdc
sasha <sasha.park.designer@gmail.com>
2025-01-14 10:55:18 +0900

    찜 슬라이스 작성

--- src/RTK/slice.js
+++ src/RTK/slice.js
@@ -29,3 +29,21 @@ export const pokemonSlice = createSlice({
 
     }
 })
+
+
+
+const favoriteSlice = createSlice({
+    name: 'favorite',
+    initialState:[1, 2, 3],
+    reducers: {
+        addToFavorite: (state, action) => {
+            state.data.push(action.payload.pokemonId)
+        },
+        removeFromFavorite: (state, action) => {
+            const index = state.indexOf(action.payload.pokemonId)
+            if (index !== -1) {
+                state.splice(index, 1)
+            }
+        }
+    } // 동기적 상태 변경
+})
